c1675f3ed22457ba16ceb6402c58269971f4f783
Elysia <2569736791@qq.com>
2025-02-24 13:56:40 +0800

    补充完成容器部分

--- src/main/java/com/elysia/elysiarecycle/guimanager/gui/RecycleGui.java
+++ src/main/java/com/elysia/elysiarecycle/guimanager/gui/RecycleGui.java
@@ -1,13 +1,22 @@
 package com.elysia.elysiarecycle.guimanager.gui;
 
+import com.elysia.elysiarecycle.ElysiaRecycle;
 import com.elysia.elysiarecycle.override.RecycleGuiHolder;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class RecycleGui {
     private Inventory inventory;
     public void create(){
         inventory = Bukkit.createInventory(new RecycleGuiHolder(), 45, "回收界面");
+        ItemStack confirmItem = new ItemStack(Material.PAPER, 1);
+        ItemMeta confirmMeta = confirmItem.getItemMeta();
+        confirmMeta.setDisplayName(ElysiaRecycle.getConfigManager().getConfigData().getTips().get("confirm").replaceAll("%count%", "0"));
+        confirmItem.setItemMeta(confirmMeta);
+        inventory.setItem(40, confirmItem);
     }
     public void openInventory(String name){
         Bukkit.getPlayer(name).openInventory(inventory);
